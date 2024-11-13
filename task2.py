00b7002a9b49f37f962c496746cf77b09ff14106
Feodosy <st086913@student.spbu.ru>
2024-11-13 16:24:17 +0000

    Algo Feodosy

--- task2.py
+++ task2.py
@@ -1,5 +1,11 @@
-"""This is my file.
-But put here your code please
-instead of this text
-and send me a pull request
-so I will be able to review your code"""
+def is_power(num1, num2):
+    ''' This function checks 
+    if the first number is the power of the second'''
+    if num1 <= 0:
+        return False
+    if num2 <= 1:
+        return num1 == 1
+    every_p = 1
+    while every_p < num1:
+        every_p *= num2
+    return every_p == num1
